c8176ac6d92d050eb54fe21823b8261a4372ef14
Dhriti <savlin21@gmail.com>
2026-07-26 16:17:16 +0530

    Implement login functionality, add session-based navbar, and create home page template

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
-from decorators import login_required
+from helpers import login_required,apology
 from datetime import timedelta
 from dotenv import load_dotenv
 
@@ -30,11 +30,35 @@ def register():
     
     username = request.form.get("username")
     pwd = request.form.get("pwd")
+    pwdC = request.form.get("pwdC")
+    if not (username and pwd and pwdC):
+        return apology("You cannot leave any of these fields empty")
+    if pwd != pwdC:
+        return apology("Password and its confirmation do not match")
     hashed_pwd = generate_password_hash(pwd)
     try:
-        cursor.execute("INSERT INTO users (username,password_hash) VALUES (?,?)",username,hashed_pwd)
-    except:
-        pass
+        cursor.execute("INSERT INTO users (username,password_hash) VALUES (?,?)",(username,hashed_pwd))
+    except sqlite3.IntegrityError:
+        return apology("Username already exists")
+    conn.commit()
+    session["user_id"] = cursor.execute("SELECT id FROM users WHERE username = ?",(username,)).fetchone()["id"]
     return redirect("/")
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    session.clear()
+    username = request.form.get("username")
+    pwd = request.form.get("pwd")
+    if not (username and pwd):
+        return apology("You cannot leave thse fields empty")
+    row = cursor.execute("SELECT id, password_hash FROM users WHERE username = ?", (username,)).fetchone()
+    if row is None or not check_password_hash(row["password_hash"],pwd):
+        return apology("Invalid username or password")
+    session["id"] = row["id"]
+    return redirect("/")
+
+    
 
 
