1f48ba7722d4f3b901d1463f0d65845cc9fb53be
Dhriti <savlin21@gmail.com>
2026-09-13 21:41:47 +0530

    Implement file hashing and redesign file managent

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import os
 import sqlite3
 import matplotlib.pyplot as plt
 import pandas as pd
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, send_file
 from werkzeug.security import check_password_hash, generate_password_hash, secure_filename
 from helpers import login_required,apology
 from datetime import timedelta
 from dotenv import load_dotenv
-
+import hashlib
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,22 +18,25 @@ graph_types = {
     1: "line",
     2: "bar"
 }
-graph_functions = {
-    "line" : line,
-    "bar" : bar
-}
-def bar():
-    return
-def line(file,filename):
+def bar(file, filename, user_id):
+    pass
+
+def line(file,filename,user_id):
     x_col = file.columns[0]
     y_col = file.columns[1]
-    ImagePath = os.path.join("uploads",str(id),"graphs",filename)
+    ImagePath = os.path.join("uploads",str(user_id),"graphs",filename)
     #Check if this image path already exists 
     plt.plot(file[x_col],file[y_col],color = "black")
     plt.xlabel(x_col)
     plt.ylabel(y_col)
     plt.savefig(ImagePath)
     plt.close()
+
+graph_functions = {
+    "line" : line,
+    "bar" : bar
+}
+
 
 @app.after_request
 def after_request(response):
@@ -43,39 +46,111 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/", methods = ["GET"])
+@app.route("/", methods = ["GET","POST"])
 @login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'GET':
+        return render_template("home.html")
+    
+    file = request.files.get("data")
+    if not file:
+        return apology("No file returned")
+    user_id = session["user_id"]
+    #Create a hash representing the data
+    file_hash = hashlib.sha256(file.read()).hexdigest()
+    file.seek(0)
+    conn = sqlite3.connect("users.db")
+    #Checking if file already exists within database
+    existing_file = conn.execute(
+    "SELECT id FROM FILES WHERE user_id = ? AND file_hash = ?",
+    (user_id, file_hash)
+).fetchone()
+
+    if not existing_file:
+        try:
+            user_folder = os.path.join("uploads",str(user_id))
+            graph_folder = os.path.join(user_folder,"graphs")
+            os.makedirs(user_folder,exist_ok=True)
+            os.makedirs(graph_folder,exist_ok=True)
+            file_name = file.filename
+            cursor = conn.execute("INSERT INTO FILES (user_id,filename,file_hash) VALUES (?,?,?)",(user_id,file_name,file_hash))
+            conn.commit()
+            file_id = cursor.lastrowid
+            file_path = os.path.join(user_folder,str(file_id))
+            file.save(file_path)
+        except OSError:
+            conn.execute("DELETE FROM FILES where id = ?",(file_id,))
+            conn.commit()
+            conn.close()
+            return apology("Upload wasn't succesful")
+        
+    else:
+        file_id = existing_file[0]
+    conn.close()
+    return render_template("graphMenu.html",file_id = file_id)
+    
 
 @app.route("/history",methods = ["GET","POST"])
 @login_required
 def history():
-    id = session["id"]
+    user_id = session["user_id"]
     conn = sqlite3.connect("users.db")
     if request.method == "POST":
         fileId = request.form.get("file")
-        graphs = conn.execute("SELECT filename FROM GRAPHS WHERE dataset_id = ?",(fileId,)).fetchall()
+        graphs = conn.execute("""
+    SELECT GRAPHS.filename
+    FROM GRAPHS
+    JOIN FILES ON GRAPHS.file_id = FILES.id
+    WHERE GRAPHS.file_id = ?
+      AND FILES.user_id = ?
+""", (fileId, user_id)).fetchall()
+        conn.close()
         return render_template("existingGraphs.html",graphs = graphs)
 
-    files = conn.execute("SELECT id,filename FROM DATASETS WHERE user_id = ? ORDER BY id",(id,))
+    files = conn.execute("SELECT id,filename FROM FILES WHERE user_id = ? ORDER BY id",(user_id,))
     return render_template("history.html",files= files.fetchall())
 
 @app.route("/generateGraph",methods = ["POST"])
 @login_required
 def generateGraph():
     graphType = int(request.form.get("graph_type"))
-    id = session["id"]
+    file_id = int(request.form.get("file_id"))
+    user_id = session["user_id"]
     conn = sqlite3.connect("users.db")
-    fileName = conn.execute("SELECT id,filename FROM DATASETS WHERE user_id = ? ORDER BY id DESC LIMIT 1",(id,)).fetchone()
-    path = "uploads/" + str(id) + "/" + fileName[1]
+    fileExists = conn.execute("SELECT id FROM FILES WHERE user_id = ? AND id = ?",(user_id,file_id)).fetchone()
+    if not fileExists:
+        return apology("ERROR")
+    path = os.path.join("uploads",str(user_id) ,str(file_id))
     file = pd.read_csv(path)
-    existing = conn.execute("SELECT filename FROM GRAPHS WHERE dataset_id = ? AND graph_type = ?",(fileName[0], graphType)).fetchone()
-    if not existing:
+    result = conn.execute("SELECT filename FROM GRAPHS WHERE file_id = ? AND graph_type = ?",(file_id, graphType)).fetchone()
+    if not result:
         graph_type = graph_types[graphType]
-        filename = fileName[1] + "_" + graph_type + ".png"
-        graph_functions[graph_type](file,filename)
-        conn.execute("INSERT INTO GRAPHS (dataset_id,graph_type,filename) VALUES (?,?,?)",(fileName[0],graphType,filename))
+        filename = str(file_id) + "_" + str(graph_type) + ".png"
+        if graph_functions[graph_type](file,filename,user_id):
+            conn.execute("INSERT INTO GRAPHS (file_id,graph_type,filename) VALUES (?,?,?)",(file_id,graphType,filename))
+            graph =  filename.removesuffix(".png")
+            conn.commit()
+        else:
+            conn.close()
+            return apology("Failed to generate graph")
+    else:
+        graph = result[0].removesuffix(".png")
+    conn.close()
+    return render_template("generateGraph.html",graph  = graph)
+
+@app.route("/graphs/<graph>")
+@login_required
+def graph(graph):
+    user_id = session["user_id"]
+    graphName = graph + ".png"
+    conn = sqlite3.connect("users.db")
+    file_id = conn.execute("SELECT file_id FROM GRAPHS WHERE filename = ?",(graphName,)).fetchone()
+    if not file_id:
+        return apology("ERROR")
+    path = os.path.join("uploads",str(user_id),"graphs",graphName)
+    conn.close()
+    return send_file(path)
+
     
         
 
@@ -83,28 +158,38 @@ def generateGraph():
     
 
 
-@app.route("/graphMenu",methods = ["POST"])
+"""@app.route("/graphMenu",methods = ["POST"])
 @login_required
 def graphMenu():
     file = request.files.get("data")
     if not file:
         return apology("No file returned")
-    id = session["id"]
-    try:
-        user_folder = "uploads/"  + str(id)
-        graph_folder = os.path.join(user_folder,"graphs")
-        os.makedirs(user_folder,exist_ok=True)
-        os.madedirs(graph_folder,exist_ok=True)
-        file_name = secure_filename(file.filename)
-        file.save(user_folder + "/" + file_name)
-    except OSError:
-        return apology("Upload wasn't succesful")
+    user_id = session["user_id"]
+    file_hash = hashlib.sha256(file.read()).hexdigest()
+    file.seek(0)
     conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO DATASETS (user_id,filename) VALUES (?,?)",(session["id"],file_name))
-    conn.commit()
+    #Checking if file already exists within database
+    existing_file = conn.execute(
+    "SELECT id FROM FILES WHERE user_id = ? AND file_hash = ?",
+    (user_id, file_hash)
+).fetchone()
+
+    if not existing_file:
+        try:
+            user_folder = "uploads/"  + str(user_id)
+            graph_folder = os.path.join(user_folder,"graphs")
+            os.makedirs(user_folder,exist_ok=True)
+            os.makedirs(graph_folder,exist_ok=True)
+            file_name = secure_filename(file.filename)
+            file.save(user_folder + "/" + file_name)
+
+        except OSError:
+            return apology("Upload wasn't succesful")
+        conn.execute("INSERT INTO FILES (user_id,filename) VALUES (?,?)",(session["user_id"],file_name))
+        conn.commit()
     conn.close()
     return render_template("graphMenu.html")
+"""
 
 
 
@@ -131,7 +216,7 @@ def register():
     except sqlite3.IntegrityError:
         return apology("Username already exists")
     conn.commit()
-    session["id"] = cursor.execute("SELECT id FROM users WHERE username = ?",(username,)).fetchone()[0]
+    session["user_id"] = cursor.execute("SELECT user_id FROM users WHERE username = ?",(username,)).fetchone()[0]
     conn.close()
     return redirect("/")
 
@@ -146,10 +231,10 @@ def login():
     pwd = request.form.get("pwd")
     if not (username and pwd):
         return apology("You cannot leave thse fields empty")
-    row = cursor.execute("SELECT id, password_hash FROM users WHERE username = ?", (username,)).fetchone()
+    row = cursor.execute("SELECT user_id, password_hash FROM users WHERE username = ?", (username,)).fetchone()
     if row is None or not check_password_hash(row[1],pwd):
         return apology("Invalid username or password")
-    session["id"] = row[0]
+    session["user_id"] = row[0]
     conn.close()
     return redirect("/")
 
